f565b4360cb32654a1ebe0bdae82799925d67a19
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-02 17:38:29 -0300

    Click score count

--- clicker/src/main.rs
+++ clicker/src/main.rs
@@ -13,11 +13,23 @@ fn conf() -> Conf {
 async fn main() {
     let (x,y) = (screen_width()/2., screen_height()/2.);
     let r = 70.;
+    let circle = Circle::new(x,y,r);
+    let mut score = 0;
     
     loop {
         clear_background(BEIGE); // Clears screen and set this color
         
         draw_circle(x, y, r, BROWN);
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let (mouse_x,mouse_y) = mouse_position();
+
+            if circle.contains(&vec2(mouse_x, mouse_y)) {
+                score += 1;
+            }
+        }
+
+        draw_text(&format!("Score: {}", score), 20.0, 20.0, 30.0, BLACK);
 
         next_frame().await; // Waits for the next frame to be rendered before continuing the loop
     }
