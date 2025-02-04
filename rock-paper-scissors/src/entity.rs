4904199e82086e98820d28f3dfa78126de069dee
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-04 16:14:48 -0300

    First movement approach: move_ent function

--- rock-paper-scissors/src/entity.rs
+++ rock-paper-scissors/src/entity.rs
@@ -1,25 +1,35 @@
 pub struct Entity {
     pos: (f32, f32),
+    orientation: (f32, f32),
+    speed: u16,
     texture_id: String,
 }
 
 impl Entity {
-    pub fn new(x: f32, y: f32, texture_id: String) -> Self {
+    pub fn new(pos: (f32, f32), orientation: (f32, f32), speed: u16, texture_id: String) -> Self {
         Entity {
-            pos: (x, y),
+            pos,
+            orientation,
+            speed,
             texture_id,
         }
     }
 
-    pub fn set_position(&mut self, x: f32, y: f32) {
-        self.pos = (x, y);
-    }
+    // GETTER & SETTER //
+    pub fn set_position(&mut self, x: f32, y: f32) { self.pos = (x, y); }
 
-    pub fn get_position(&self) -> (f32, f32) {
-        self.pos
-    }
+    pub fn get_position(&self) -> (f32, f32) { self.pos }
 
-    pub fn get_texture_id(&self) -> &str {
-        &self.texture_id
-    }
+    pub fn set_orientation(&mut self, x: f32, y: f32) { self.orientation = (x, y); }
+
+    pub fn get_orientation(&self) -> (f32, f32) { self.orientation }
+
+    pub fn set_speed(&mut self, speed: u16) { self.speed = speed; }
+
+    pub fn get_speed(&self) -> u16 { self.speed }
+
+    pub fn set_texture_id(&mut self, texture_id: String) { self.texture_id = texture_id; }
+
+    pub fn get_texture_id(&self) -> &str { &self.texture_id }
+    // GETTER & SETTER //
 }
--- rock-paper-scissors/src/main.rs
+++ rock-paper-scissors/src/main.rs
@@ -5,6 +5,26 @@ use macroquad::prelude::*;
 use entity::Entity;
 use texture_handler::TextureHandler;
 
+pub enum MovementType {
+    LINEAR,
+}
+
+pub fn move_ent(entity: &mut Entity, movement: MovementType) {
+    let (curr_x, curr_y) = entity.get_position();
+    let (oriented_x, oriented_y) = entity.get_orientation();
+    let speed = entity.get_speed() as f32;
+    let delta = get_frame_time();
+
+    match movement {
+        MovementType::LINEAR => {
+            let updated_x = curr_x + delta * speed * oriented_x;
+            let updated_y = curr_y + delta * speed * oriented_y;
+            entity.set_position(updated_x, updated_y);
+        },
+    }
+}
+
+
 fn conf() -> Conf {
     Conf {
       window_title: "Rock - Paper - Scissors".to_owned(),
@@ -15,21 +35,18 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
-    let mut x = 0.;
-    let mut y = 0.;
     let texture: Texture2D = load_texture("assets/images/test.png").await.unwrap();
 
-    let mut ente = Entity::new(x, y, String::from("test"));
-    let mut handler = TextureHandler::new();
-    handler.add_texture("test", texture);
+    let mut ente = Entity::new((0., 0.), (1., 1.), 100, String::from("test"));
+
+    let mut texture_handler = TextureHandler::new();
+    texture_handler.add_texture("test", texture);
 
     loop {
         clear_background(BEIGE);
 
-        x += 1.;
-        y += 1.;
-        ente.set_position(x, y);
-        handler.draw(&ente);
+        move_ent(&mut ente, MovementType::LINEAR);
+        texture_handler.draw(&ente);
 
         next_frame().await;
     }
