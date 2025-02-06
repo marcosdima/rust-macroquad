b018381914152374e392fb514006e3e62c971ffe
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-06 11:58:29 -0300

    Degrees struct instead of Orientation and MovementHandler to manage entity trayectory

--- rock-paper-scissors/src/entity.rs
+++ rock-paper-scissors/src/entity.rs
@@ -1,3 +1,6 @@
+use macroquad::rand;
+
+#[derive(Debug, Copy, Clone)]
 pub struct Coords {
     x: f32,
     y: f32,
@@ -23,54 +26,133 @@ impl Coords {
     pub fn y(&self) -> f32 { self.y }
 }
 
-pub struct Orientation {
-    to: Coords,
+#[derive(Debug, Copy, Clone)]
+pub struct Degrees(f32);
+
+impl Degrees {
+    pub fn new(angle: f32) -> Self {
+        Self(angle.rem_euclid(360.0)) 
+    }
+
+    pub fn to_radians(self) -> f32 {
+        self.0.to_radians()
+    }
+
+    pub fn add(self, other: f32) -> Self {
+        Self::new(self.0 + other)
+    }
+
+    pub fn sub(self, other: f32) -> Self {
+        Self::new(self.0 - other)
+    }
+}
+pub enum MovementType {
+    LINEAR,
+    RANDOM,
+    STATIC,
 }
 
-impl Orientation {
-    pub fn new(x: f32, y: f32) -> Self {
-        if x < -1. || x > 1. {
-            panic!("X ({x}) must be a number between -1 and 1.");
-        }
-        else if y < -1. || y > 1. {
-            panic!("Y ({y}) must be a number between -1 and 1.");
-        } else {
-            Orientation { to: Coords::new(x, y) }
+struct MovementHandler {
+    movement_type: MovementType,
+    trayectory: Box<dyn FnMut(f32) -> Coords>,
+    counter: u32,
+}
+
+impl MovementHandler {
+    pub fn new(movement_type: MovementType) -> Self {
+        MovementHandler {
+            movement_type,
+            trayectory: Box::new(move |_e: f32| { Coords::new(0., 0.) }),
+            counter: 0,
         }
     }
 
-    pub fn set(&mut self, x: f32, y: f32) { self.to.set(x, y); }
+    pub fn update_trayectory(&mut self, point: Coords, angle: Degrees) {
+        self.counter = 0;
+        let (curr_x, curr_y) = point.pair();
+        let r = angle.to_radians();
 
-    pub fn get(&self) -> &Coords { &self.to }
+        match self.movement_type {
+            MovementType::LINEAR => {
+                let linear_movement = move |t: f32| -> Coords {
+                    Coords {
+                        x: curr_x + t * r.cos(),
+                        y: curr_y - t * r.sin()
+                    }
+                };
+        
+                self.trayectory = Box::new(linear_movement);
+            },
+            MovementType::RANDOM => {
+                let mut x = curr_x;
+                let mut y = curr_y;
+
+                self.trayectory = Box::new(
+                    move |_t: f32| -> Coords {
+                        let bool = rand::gen_range(0, 2);
+
+                        if bool == 0 {
+                            x += r.cos();
+                        } else {
+                            y -= r.sin();
+                        }
+
+                        Coords { x, y }
+                    }
+                );
+            }
+            _ => {}
+        }
+    }
+
+    pub fn get_next_coords(&mut self) -> Coords {
+        self.counter += 1;
+        (self.trayectory)(self.counter as f32)
+    } 
 }
 
 pub struct Entity {
     pos: Coords,
-    orientation: Orientation,
+    orientation: Degrees,
     speed: u16,
     texture_id: String,
+    movement_handler: MovementHandler,
 }
 
 // TOTHINK: Unidad de movimiento que se consume.
 
 impl Entity {
-    pub fn new(pos: Coords, orientation: Orientation, speed: u16, texture_id: String) -> Self {
-        Entity {
+    pub fn new(pos: Coords, orientation: Degrees, speed: u16, texture_id: String, movement_type: MovementType) -> Self {
+        let mut ent = Entity {
             pos,
             orientation,
             speed,
             texture_id,
-        }
+            movement_handler: MovementHandler::new(movement_type),
+        };
+
+        // Updates trayectory.
+        ent.set_orientation(orientation);
+
+        ent
     }
 
     // GETTER & SETTER //
     pub fn set_position(&mut self, x: f32, y: f32) { self.pos.set(x, y); }
 
-    pub fn get_position(&self) -> &Coords { &self.pos }
+    pub fn get_position(&self) -> Coords { self.pos }
 
-    pub fn set_orientation(&mut self, x: f32, y: f32) { self.orientation.set(x, y); }
+    pub fn set_orientation(&mut self, degrees: Degrees) {
+        self.orientation = degrees;
 
-    pub fn get_orientation(&self) -> &Orientation { &self.orientation }
+        let point = self.get_position();
+        let angle = self.get_orientation();
+        let movement_handler = &mut self.movement_handler;
+        
+        movement_handler.update_trayectory(point, angle);
+    }
+
+    pub fn get_orientation(&self) -> Degrees { self.orientation }
 
     pub fn set_speed(&mut self, speed: u16) { self.speed = speed; }
 
@@ -80,4 +162,6 @@ impl Entity {
 
     pub fn get_texture_id(&self) -> &str { &self.texture_id }
     // GETTER & SETTER //
+
+    pub fn move_body(&mut self) { self.pos = self.movement_handler.get_next_coords(); } 
 }
--- rock-paper-scissors/src/main.rs
+++ rock-paper-scissors/src/main.rs
@@ -5,34 +5,6 @@ use macroquad::prelude::*;
 use entity::*;
 use texture_handler::TextureHandler;
 
-pub enum MovementType {
-    LINEAR,
-    RANDOM,
-}
-
-pub fn move_ent(entity: &mut Entity, movement: MovementType) {
-    let (curr_x, curr_y) = entity.get_position().pair();
-    let (oriented_x, oriented_y) = entity.get_orientation().get().pair();
-    let speed = entity.get_speed() as f32;
-    let delta = get_frame_time();
-
-    let updated_x = curr_x + delta * speed * oriented_x;
-    let updated_y = curr_y + delta * speed * oriented_y;
-
-    match movement {
-        MovementType::LINEAR => {
-            entity.set_position(updated_x, updated_y);
-        },
-        MovementType::RANDOM => {
-            let bool = rand::gen_range(0, 2);
-            entity.set_position(
-                if bool == 0 { updated_x } else { curr_x }, 
-                if bool > 0 { updated_y} else { curr_y }
-            );
-        }
-    }
-}
-
 fn conf() -> Conf {
     Conf {
       window_title: "Rock - Paper - Scissors".to_owned(),
@@ -45,24 +17,31 @@ fn conf() -> Conf {
 async fn main() {
     let texture: Texture2D = load_texture("assets/images/test.png").await.unwrap();
 
-    let pos = Coords::new(0., 0.);
-    let orientation = Orientation::new(1., 1.);
-    let mut ente = Entity::new(pos, orientation, 50, String::from("test"));
+    let pos = Coords::new(200., 200.);
+    let orientation = Degrees::new(290.);
+    let mut ente = Entity::new(pos, orientation, 50, String::from("test"), MovementType::RANDOM);
 
-    let pos = Coords::new(500., 500.);
-    let orientation = Orientation::new(-1., -1.);
-    let mut ente2: Entity = Entity::new(pos, orientation, 50, String::from("test"));
+    //let pos = Coords::new(500., 500.);
+    //let orientation = Degrees::new(75.);
+    //let mut ente2: Entity = Entity::new(pos, orientation, 50, String::from("test"), MovementType::LINEAR);
 
     let mut texture_handler = TextureHandler::new();
     texture_handler.add_texture("test", texture);
 
+    let mut counter = 0;
+
     loop {
         clear_background(BEIGE);
 
-        move_ent(&mut ente, MovementType::RANDOM);
-        move_ent(&mut ente2, MovementType::RANDOM);
+        ente.move_body();
+        //ente2.move_body();
         texture_handler.draw(&ente);
-        texture_handler.draw(&ente2);
+        //texture_handler.draw(&ente2);
+
+        counter += 1;
+        if counter == 100 {
+            ente.set_orientation(Degrees::new(45.));
+        }
 
         next_frame().await;
     }
